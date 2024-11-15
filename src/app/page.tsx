40d03b9e462c928f6bf1c6efd6b110df884650d3
Douglas Nicida <douglasnicida@gmail.com>
2024-11-15 14:28:23 -0300

    [obs] - add loading observation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -106,7 +106,7 @@ useEffect(() => {
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
             <Suspense fallback={<FallBackSkeleton />}>
             {
-              loading ? <p>
+              loading ? <p className="w-full">
                 <span className="animate-pulse">Loading...</span> (pode ser que demore cerca de 50s na primeira vez depois de um tempo por conta do modo hibernação do servidor em que o projeto 
                 foi hospedado)</p> : 
               trendingAnimes.map((anime, index) => {
